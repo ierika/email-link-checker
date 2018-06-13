ebb998953a8908943aac00fe30b945a479801ce6
Erika Suzuki <erika.suzuki@springernature.com>
2018-06-13 14:47:01 +0900

    update tool: accept multiple files

--- linkchecker.py
+++ linkchecker.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env Python3
 
-'''
+"""
 HTML and text email link checker.
 
 It only looks for absolute/full URL's in an HTML web page or text file.
@@ -9,7 +9,7 @@ Any HTTP response that is not an OK response (HTTP 200) will be considered,
 an error.
 All URLs that had error will be automatically opened in a browser...
 so it can be double checked.
-'''
+"""
 
 import sys
 import os
@@ -51,97 +51,114 @@ class Main:
     allowed_protocols = ('http://', 'https://')
     allowed_mimetypes = ('text/plain', 'text/html')
 
-    def __init__(self, args):
-        urls = None
-
-        try:
-            args = args[1]
-        except:
-            print(Prompter('Please provide an URL or a file').warning())
-            exit(1)
-
+    def __init__(self, args=None):
+        # Check requirements
         self.check_requirements()
 
-        if args.startswith(self.allowed_protocols):
-            # Process URL
-            print(Prompter('Checking links from a URL').message())
-            html = self.get_url_content(args)
-            urls = self.parse_html_urls(html)
-        elif os.path.isfile(args):
-            # Process files
-            mime = self.guess_mimetype(args)
-            print(
-                Prompter(
-                    'Checking links from a {} file'.format(mime)
-                ).message()
-            )
+        # If the user provided no arguments, spit out an error.
+        # If arguments were provided slice the first argument off.
+        # The first argument will be this script and we don't need that
+        if len(args) <= 1:
+            print(Prompter('Please provide an URL or a file').warning())
+            exit(1)
+        else:
+            args = args[1:]
 
-            with open(args, 'r') as f:
-                file_contents = f.read()
+        for arg in args:
+            # Reset URL list on each iteration
+            urls = None
+            if arg.startswith(self.allowed_protocols):
+                # Process URL
+                print(Prompter('Checking links from a URL').message())
+                html = self.get_url_content(arg)
+                urls = self.parse_html_urls(html)
+            elif os.path.isfile(arg):
+                # Process files
+                mime, encoding = self.guess_mimetype(arg)
+                print(
+                    Prompter(
+                        'Checking links from a {} file: {}'.format(mime, arg)
+                    ).message()
+                )
 
-            if mime == self.allowed_mimetypes[0]:  # text/plain
-                urls = self.parse_text_urls(file_contents)
-            elif mime == self.allowed_mimetypes[1]:  # text/html
-                urls = self.parse_html_urls(file_contents)
+                with open(arg, 'r', encoding="utf-8") as f:
+                    file_contents = f.read()
+
+                if mime == self.allowed_mimetypes[0]:  # text/plain
+                    urls = self.parse_text_urls(file_contents)
+                elif mime == self.allowed_mimetypes[1]:  # text/html
+                    urls = self.parse_html_urls(file_contents)
+                else:
+                    print(
+                        Prompter(
+                            '{} mimetype not allowed.'.format(mime)
+                        ).error()
+                    )
+                    exit(1)
             else:
                 print(
-                    Prompter('{} mimetype not allowed.'.format(mime)).error()
+                    Prompter(
+                        '{} is not a valid URL or file.'.format(arg)
+                    ).error()
                 )
-                exit(1)
-        else:
-            print(
-                Prompter('{} is not a valid URL or file.'.format(args)).error()
-            )
-            exit(1)
 
-        print(Prompter('Checking {} URL(s)...'.format(len(urls))).message())
-        self.check_urls(urls)
-        print(Prompter('Done').message())
+            if urls:
+                print(Prompter(
+                    'Checking {} URL(s)...'.format(len(urls))
+                ).message())
+                self.check_urls(urls)
+                print(Prompter('Done').message())
+                print()  # An empty line
 
-    def guess_mimetype(self, file):
-        '''
+    @staticmethod
+    def guess_mimetype(file):
+        """
         Guesses a file's mimetype and encoding.
-        '''
+        """
         mime, encoding = mimetypes.guess_type(file)
-        return mime
+        return mime, encoding
 
-    def get_url_content(self, url):
-        '''
+    @staticmethod
+    def get_url_content(url):
+        """
         Checks an URL whether it's returning an OK response or not.
 
         Anything other than HTTP 200 is considered not ok.
-        '''
+        """
         html = None
+        # noinspection PyBroadException
         try:
             html = urlopen(url)
-        except KeyboardInterrupt as e:
+        except KeyboardInterrupt:
             print(Prompter('Aborting...').error())
             exit(2)
-        except:
+        except Exception:
             webbrowser.open_new_tab(url)
             return None
         return html
 
-    def check_requirements(self):
-        '''
+    @staticmethod
+    def check_requirements():
+        """
         Checks whether the script is compatible with the OS
-        '''
+        """
         if os.name is not 'posix':
             print('This script currently only supports Unix-like OS\'s.')
             exit(1)
 
-    def clear_screen(self):
-        '''
+    @staticmethod
+    def clear_screen():
+        """
         Clears console screen
-        '''
+        """
         os.system('clear')
 
     def check_urls(self, urls):
-        '''
+        """
         Checks each URLs from an URL set.
 
         Sorted just to make it look organized.
-        '''
+        """
         for url in urls:
             html = self.get_url_content(url)
             if html:
@@ -150,11 +167,11 @@ class Main:
                 print(Prompter(url).error())
 
     def parse_html_urls(self, file_contents):
-        '''
+        """
         Looks for all anchor tags with an absolute URL in an HTML doc.
-        '''
-        bsObj = BeautifulSoup(file_contents, "html.parser")
-        bs_links = bsObj.findAll('a')
+        """
+        soup = BeautifulSoup(file_contents, "html.parser")
+        bs_links = soup.findAll('a')
         urls = []
         if len(bs_links) > 0:
             for bs_link in bs_links:
@@ -168,10 +185,11 @@ class Main:
             exit(1)
         return tuple(urls)
 
-    def parse_text_urls(self, file_contents):
-        '''
+    @staticmethod
+    def parse_text_urls(file_contents):
+        """
         Parses text file for absolute URLs
-        '''
+        """
         pattern = r'((https|http)://[\w\d\.\=\#\_\+\!\&\%\/\?\-]+)'
         matches = re.findall(pattern, file_contents)
         urls = []
